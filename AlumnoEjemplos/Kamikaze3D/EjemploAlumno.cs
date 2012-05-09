985397f57b38e367c79862adfc3e5895a4597d26
fede@federicog.com.ar <fede@federicog.com.ar@66c0d3e7-41d2-30ad-5e6e-0780517c3a61>
2012-05-09 18:25:09 +0000

    Escenario con múltiples cuadras.

--- AlumnoEjemplos/Kamikaze3D/EjemploAlumno.cs
+++ AlumnoEjemplos/Kamikaze3D/EjemploAlumno.cs
@@ -48,18 +48,12 @@ namespace AlumnoEjemplos.Kamikaze3D
         #endregion
 
         #region Variables Globales
-        //VoidMap map;
-        //Vector3 lightPosition;
-        //List<VoidBloque> cuadras;
-        TgcScene scene;
-        TgcFrustum frustum;
+        Escenario escenario;
         #endregion
 
         public EjemploAlumno()
         {
-            //this.map = new VoidMap();
-            this.frustum = GuiController.Instance.Frustum;
-            //LightPosition = new Vector3(-4000, 8000, -1000);
+            this.escenario = new Escenario();
         }
 
         /// <summary>
@@ -68,46 +62,8 @@ namespace AlumnoEjemplos.Kamikaze3D
         /// </summary>
         public override void init()
         {
-            /*
-            this.map.iniciar();
-            this.map.setCamaraPosition = GuiController.Instance.ThirdPersonCamera.Position;
-            this.map.setLightPosition = this.lightPosition;
-            this.map.moveDefaultPosition();
-             
-            //Crea una lista de los bloques a renderizar y se la mando al mapa para que se encarge de actualizarle valores internos y luego renderizar
-            this.cuadras = new List<VoidBloque>();
-
-            foreach (VoidBloque Cuadra in this.map.blocks)
-            {
-                //Agrega las cuadras que colisionan contra el frustum
-                TgcCollisionUtils.FrustumResult Result = TgcCollisionUtils.classifyFrustumAABB(this.frustum, Cuadra.structure[0].BoundingBox);
-                if (Result != TgcCollisionUtils.FrustumResult.OUTSIDE)
-                {
-                    this.cuadras.Add(Cuadra);
-                    GuiController.Instance.UserVars.setValue("Meshes renderizadas", this.cuadras.Count);
-                }
-            }
-            */
-
-            GuiController.Instance.UserVars.addVar("Meshes renderizadas");
-            GuiController.Instance.UserVars.addVar("Meshes no renderizadas");
-
-            GuiController.Instance.FpsCamera.Enable = true;
-            GuiController.Instance.FpsCamera.setCamera(new Vector3(-140f, 40f, -50f), new Vector3(-140f, 40f, -120f));
-            GuiController.Instance.FpsCamera.MovementSpeed = 200f;
-            GuiController.Instance.FpsCamera.JumpSpeed = 200f;
-            
-            //Crear loader
-            TgcSceneLoader loader = new TgcSceneLoader();
-
-            //Configurar MeshFactory customizado
-            loader.MeshFactory = new MyCustomMeshFactory();
-
-            //Carga el archivo del bloque
-            this.scene = loader.loadSceneFromFile(GuiController.Instance.AlumnoEjemplosMediaDir + "Kamikaze3D\\Block\\Block-TgcScene.xml");
-        
+            this.escenario.init();
         }
-
 
         /// <summary>
         /// Método que se llama cada vez que hay que refrescar la pantalla.
@@ -116,34 +72,7 @@ namespace AlumnoEjemplos.Kamikaze3D
         /// <param name="elapsedTime">Tiempo en segundos transcurridos desde el último frame</param>
         public override void render(float elapsedTime)
         {
-
-            //this.map.renderList(ref this.cuadras);
-
-            int render = 0;
-            int noRender = 0;
-            foreach (TgcMesh mesh in this.scene.Meshes)
-            {
-                //Nos ocupamos solo de las mallas habilitadas
-                if (mesh.Enabled)
-                {
-                    //Solo mostrar la malla si colisiona contra el Frustum
-                    TgcCollisionUtils.FrustumResult r = TgcCollisionUtils.classifyFrustumAABB(this.frustum, mesh.BoundingBox);
-                    if (r != TgcCollisionUtils.FrustumResult.OUTSIDE)
-                    {
-                        mesh.render();
-                        render++;
-                    }
-                    else
-                    {
-                        noRender++;
-                    }
-                }
-            }
-            GuiController.Instance.UserVars.setValue("Meshes renderizadas", render);
-            GuiController.Instance.UserVars.setValue("Meshes no renderizadas", noRender);
-
-
-
+            this.escenario.render(elapsedTime);
         }
 
         /// <summary>
@@ -152,7 +81,7 @@ namespace AlumnoEjemplos.Kamikaze3D
         /// </summary>
         public override void close()
         {
-            this.scene.disposeAll();
+            this.escenario.close();
         }
 
     }
