441c3b1ecaf93e35aeb0e4223807abbfa19bd2c2
gustavo.ggp11@gmail.com <gustavo.ggp11@gmail.com@66c0d3e7-41d2-30ad-5e6e-0780517c3a61>
2012-05-12 18:27:11 +0000

    Agregado del arma al personaje. Aparece todo el tiempo y apunta con E.

---
+++ trunk/AlumnoEjemplos/Test/Personaje.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TgcViewer.Utils.TgcSkeletalAnimation;
+using TgcViewer.Utils.TgcSceneLoader;
+using Microsoft.DirectX;
+using Microsoft.DirectX.Direct3D;
+
+namespace AlumnoEjemplos.Test
+{
+    class Personaje : TgcSkeletalMesh
+    {
+        public static void addWeapon(TgcSkeletalMesh personaje, Weapon aWeapon)
+        {
+            List<TgcMesh> listMesh = aWeapon.getListMesh();
+
+            foreach (TgcMesh m in listMesh)
+            {
+                TgcSkeletalBoneAttach attachment = new TgcSkeletalBoneAttach();
+                attachment.Mesh = m;           
+                attachment.Bone = personaje.getBoneByName("Bip01 R Hand");
+                //attachment.Offset = Matrix.Translation(0, -10, -15);
+
+                Matrix resultMatrix = Matrix.Translation(-5, -10, -15);
+                resultMatrix = Matrix.Multiply(resultMatrix, Matrix.Scaling(0.5f, 0.5f, 0.5f));
+                resultMatrix = Matrix.Multiply(resultMatrix, Matrix.RotationY(Geometry.RadianToDegree(90)));
+                //resultMatrix = Matrix.Multiply(resultMatrix, Matrix.Translation(0, 10, 0));
+                resultMatrix = Matrix.Multiply(resultMatrix, Matrix.Translation(5, 0, 0));
+                //resultMatrix = Matrix.Multiply(resultMatrix, Matrix.RotationZ(Geometry.DegreeToRadian(30)));
+                //resultMatrix = Matrix.Multiply(resultMatrix, Matrix.RotationX(Geometry.RadianToDegree(20)));
+                attachment.Offset = resultMatrix;
+                //attachment.Offset = Matrix.Multiply(Matrix.Multiply( 
+                //                                        Matrix.Translation(0, -10, -15), 
+                //                                        Matrix.Scaling(0.5f, 0.5f, 0.5f)),
+                //                                    Matrix.RotationY(Geometry.RadianToDegree(90)));
+                attachment.updateValues();
+                personaje.Attachments.Add(attachment);
+            }
+        }
+    }
+}
