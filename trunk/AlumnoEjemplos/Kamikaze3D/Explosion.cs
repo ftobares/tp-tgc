fb1b845620ccba5cab99ac6e2f6439ddda6e555d
fede@federicog.com.ar <fede@federicog.com.ar@66c0d3e7-41d2-30ad-5e6e-0780517c3a61>
2012-06-05 18:00:26 +0000

    Intento de phong shading para la explosión.

--- trunk/AlumnoEjemplos/Kamikaze3D/Explosion.cs
+++ trunk/AlumnoEjemplos/Kamikaze3D/Explosion.cs
@@ -3,11 +3,11 @@ using System.Drawing;
 using System.Collections.Generic;
 using System.Text;
 using Microsoft.DirectX;
+using Microsoft.DirectX.Direct3D;
 using TgcViewer;
 using TgcViewer.Utils.Modifiers;
 using TgcViewer.Utils.TgcGeometry;
 using TgcViewer.Utils.TgcSceneLoader;
-using TgcViewer.Utils.TgcGeometry;
 
 namespace AlumnoEjemplos.Kamikaze3D
 {
@@ -26,18 +26,34 @@ namespace AlumnoEjemplos.Kamikaze3D
         private float cameraAcceleration = 0.5F;
         private Camara camara;
 
+        private Effect effect;
+
         private bool detonada = false;
 
         public void init(Camara camara)
         {
+            this.camara = camara;
+            
             //Crear loader
             TgcSceneLoader loader = new TgcSceneLoader();
+            //Configurar MeshFactory customizado
+            loader.MeshFactory = new MyCustomMeshFactory();
             this.scene = loader.loadSceneFromFile(GuiController.Instance.AlumnoEjemplosMediaDir + "Kamikaze3D\\Explosion\\Explosion-TgcScene.xml");
 
-            this.camara = camara;
+            //Cargar shader
+            Device d3dDevice = GuiController.Instance.D3dDevice;
+            string compilationErrors;
+            this.effect = Effect.FromFile(d3dDevice, GuiController.Instance.AlumnoEjemplosMediaDir + "Kamikaze3D\\Shaders\\Explosion.fx", null, null, ShaderFlags.None, null, out compilationErrors);
+            if (this.effect == null)
+                throw new Exception("Error al cargar shader. Errores: " + compilationErrors);
+           
+            this.effect.Technique = "DefaultTechnique";
 
-            foreach (TgcMesh mesh in this.scene.Meshes)
+            foreach (MyMesh mesh in this.scene.Meshes)
+            {
                 mesh.Scale = new Vector3(0, 0, 0);
+                mesh.effect = this.effect;
+            }
         }
 
         public void render(float elapsedTime)
@@ -63,10 +79,11 @@ namespace AlumnoEjemplos.Kamikaze3D
                 //Generar explosión escalando scene
                 float scale = (float)(0.1 * (this.expTime * this.speed + Math.Pow(this.expTime, 2) * this.acceleration));
 
+                Vector3 position = this.camara.getPosition();
+
                 if (scale <= this.maxScale)
                 {
 
-                    Vector3 position = this.camara.getPosition();
                     position = position - new Vector3(0, position.Y, -100);
 
                     foreach (TgcMesh mesh in this.scene.Meshes)
@@ -76,7 +93,23 @@ namespace AlumnoEjemplos.Kamikaze3D
                     }
 
                 }
-                this.scene.renderAll();
+
+                //Configurar shader
+                this.effect.Technique = "DefaultTechnique";
+                this.effect.SetValue("fvLightPosition", TgcParserUtils.vector3ToFloat3Array(position));
+                this.effect.SetValue("fvEyePosition", TgcParserUtils.vector3ToFloat3Array(position));
+                this.effect.SetValue("k_la", 0.5f);
+                this.effect.SetValue("k_ld", 0.6f);
+                this.effect.SetValue("k_ls", 0.5f);
+                this.effect.SetValue("fSpecularPower", 16f);
+
+                Device device = GuiController.Instance.D3dDevice;
+                //device.Clear(ClearFlags.Target | ClearFlags.ZBuffer, Color.Black, 1.0f, 0);
+                foreach (MyMesh m in this.scene.Meshes)
+                {
+                    m.effect = effect;
+                    m.render();
+                }
 
             }
 
@@ -91,6 +124,7 @@ namespace AlumnoEjemplos.Kamikaze3D
         public void close()
         {
             this.scene.disposeAll();
+            this.effect.Dispose();
         }
 
         public bool estaEjecutandose()
